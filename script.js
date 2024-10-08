c3716b8a2b3aab40aa267134e84a8c2c26957db2
Daniel Falone <d.falone.melo@gmail.com>
2024-10-07 21:08:10 -0300

    Teste de strings

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Conversão de string para numero");
+
+const numero = parseInt("27");
+console.log(numero);
+
+console.log("Conversão de string para real");
+
+const numero2 = parseFloat("27.30");
+console.log(numero2);
+
+console.log("Conversão para letras minusculas");
+
+const nome = "Daniel Falone  ";
+console.log(`const nome = "Daniel Falone "`);
+const resultado = nome.toLowerCase();
+console.log(resultado);
+
+
+
+
+
